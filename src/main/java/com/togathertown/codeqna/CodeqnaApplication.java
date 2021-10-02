c2cd43276896d115eb35f85ba305592f67e23f85
starkensin <starkensin@gmail.com>
2021-10-02 19:51:45 +0900

    init spring-boot

---
+++ src/main/java/com/togathertown/codeqna/CodeqnaApplication.java
@@ -0,0 +1,13 @@
+package com.togathertown.codeqna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeqnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeqnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/togathertown/codeqna/CodeqnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togathertown.codeqna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeqnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
